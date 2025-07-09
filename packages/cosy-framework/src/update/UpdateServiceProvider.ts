61fa5f930da6770cc58cd6b5de0890c58ea2d0e5
Nookery <nooks@qq.com>
2025-07-09 23:22:42 +0800

    🔧 Config: Move window contracts to framework and update related files

--- packages/cosy-framework/src/update/UpdateServiceProvider.ts
+++ packages/cosy-framework/src/update/UpdateServiceProvider.ts
@@ -1,21 +1,14 @@
 import { ServiceProvider } from '../setting/ServiceProvider.js';
 import { UpdateManager } from './UpdateManager.js';
 import { IApplication } from '../contract/IApplication.js';
-import { ConfigManager } from '../config/types.js';
 import { ILogManager } from '../contract/logger/ILogManager.js';
-import {
-  AppAbstract,
-  ConfigAbstract,
-  LogAbstract,
-  UpdateContract,
-} from '../constants.js';
+import { AppAbstract, LogAbstract, UpdateContract } from '../constants.js';
 
 export class UpdateServiceProvider extends ServiceProvider {
   public register(): void {
     this.app.singleton(UpdateContract, () => {
       return new UpdateManager(
         this.app.make<IApplication>(AppAbstract),
-        this.app.make<ConfigManager>(ConfigAbstract),
         this.app.make<ILogManager>(LogAbstract)
       );
     });
