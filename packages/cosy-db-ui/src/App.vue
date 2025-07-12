9efe58838fbe26e5ad83918e00fb202da9192f7d
Nookery <nooks@qq.com>
2025-07-12 17:36:52 +0800

    🗑️ Trash: Remove deprecated cosy-db-ui package and test files

--- packages/cosy-db-ui/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="app" class="min-h-screen bg-base-100">
-    <DatabaseUI />
-  </div>
-</template>
-
-<script setup lang="ts">
-import DatabaseUI from './components/DatabaseUI.vue'
-</script>
--- packages/cosy-db-ui/src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Cosy Database UI Extension
- *
- * 提供数据库管理的Web界面
- */
-
-export { DatabaseUIServer } from './server/DatabaseUIServer.js';
-export { type DatabaseUIServerConfig } from './server/DatabaseUIServer.js';
-export { default as configureDbUICommand } from './commands/ui-command.js';
