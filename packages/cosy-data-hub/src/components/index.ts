fe823e7354231fe4628b85220fb9e1f871940862
Nookery <nooks@qq.com>
2025-07-13 19:43:31 +0800

    🐎 Improve: Refactor layout structure and update dependencies

--- packages/cosy-data-hub/src/components/index.ts
+++ packages/cosy-data-hub/src/components/index.ts
@@ -1,5 +1,3 @@
-// 组件索引文件
-export { default as Layout } from './Layout.astro';
 export { default as ConnectionConfig } from './ConnectionConfig.astro';
 export { default as TablesList } from './TablesList.astro';
 export { default as TableData } from './TableData.astro';
